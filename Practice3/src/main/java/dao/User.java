e50e21a9a8cf0f70ebc9e0de032ba4ce850a7276
yaox123 <974526541@qq.com>
2019-06-22 13:25:54 +0800

    第一次测试

---
+++ Practice3/src/main/java/dao/User.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.util.Date;
+
+/**
+ * @BelongsProject: Practice
+ * @BelongsPackage: dao
+ * @Author: yaox
+ * @CreateTime: 2019-05-30 22:32
+ */
+public class User {
+    /**
+     * 年龄
+     */
+    Integer age;
+
+    /**
+     * 姓名
+     */
+    String name;
+
+    /**
+     * 出生日期
+     */
+    Date birth;
+}
